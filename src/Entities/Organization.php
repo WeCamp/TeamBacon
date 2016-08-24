88514a596a41b47ae1e241c4e39e497ecf5bf8cd
Vitalii Levchenko <vitalii.levchenko@gmail.com>
2016-08-24 12:43:51 +0200

    Added getters and setters to organization entity

--- src/Entities/Organization.php
+++ src/Entities/Organization.php
@@ -11,9 +11,14 @@ use GraphAware\Neo4j\OGM\Annotations as OGM;
 final class Organization
 {
     /**
-     * @OGM\GraphId()
+     * @OGM\Property(type="int")
      */
     protected $id;
+
+    /**
+     * @OGM\Property(type="string")
+     */
+    private $name;
 
     /**
      * @OGM\Property(type="string")
@@ -24,4 +29,110 @@ final class Organization
      * @OGM\Property(type="string")
      */
     private $description;
+
+    /**
+     * @OGM\Property(type="string")
+     */
+    private $url;
+
+    /**
+     * @OGM\Property(type="string")
+     */
+    private $location;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlog()
+    {
+        return $this->blog;
+    }
+
+    /**
+     * @param string $blog
+     */
+    public function setBlog($blog)
+    {
+        $this->blog = $blog;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
 }
