7d73f31ba9bd639be6eb7b01359c168adccc7c3e
PHPetra <dreis@xs4all.nl>
2016-08-24 18:33:57 +0200

    created cli command infrastructure and readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Team Bacon
+
+Some hopefully useful documentation.
+
+## CLI script 
+There's a command line script to import data from github into the graph: `scripts/cli`.
+In order to use it you first have to make it executable, like so
+    
+    chmod a+x scripts/cli
+
+You can call it like this: 
+
+    ./scripts/cli bacon:import-github
+
+It requires a few parameters, see in `src/Console/Import2GraphCommand.php`
---
+++ src/Console/Import2GraphCommand.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types = 1);
+
+namespace Bacon\Console;
+
+
+use Bacon\Service\Crawler\Dto\User;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Class Import2GraphCommand
+ * Imports DTO objects from Github or other sources into the Graph
+ *
+ */
+class Import2GraphCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('bacon:import-github')
+            ->setDescription('import objects from github')
+            ->setHelp('Imports objects from github into the Graph and transforms the DTO\'s to OGM ')
+            ->addArgument('object', InputArgument::REQUIRED,
+                'Please provide an object to import. Available options: user|repository|Language ...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $object = $input->getArgument('object');
+
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'Importing ' . $object,
+            '============',
+        ]);
+
+
+        if ('user' === $object) {
+
+
+        }
+
+
+        $output->writeln('All done.');
+    }
+
+
+    /**
+     * Fetch WeCamp members from cache or GitHub API
+     *
+     * @param OutputInterface $output
+     * @return \Bacon\Service\Crawler\Bags\UserBag
+     */
+    private function handleUsers(OutputInterface $output)
+    {
+        $output->writeln('Fetching user details github api.');
+
+        $controller = new \Bacon\Service\Crawler\Controllers\CrawlerController();
+        $org = $controller->getData();
+        $users = $org->getMembers();
+        $graphUsers = [];
+
+        foreach ($users as $user) {
+            $graphUsers[] = $this->transformUser($user);
+        }
+    }
+
+    /**
+     * Trnsforms a DTO User into a Graph Node
+     *
+     * @param User $user
+     */
+    private function transformUser(User $user)
+    {
+        $node = new \Bacon\Entity\User();
+    }
+}
