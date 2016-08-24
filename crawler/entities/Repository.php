e93791ad546507645ff03c6ad78d29a5c0c05f3f
chris <chris@dfg.gd>
2016-08-24 11:39:49 +0200

    Add crawlers entity classes

---
+++ crawler/entities/Repository.php
@@ -0,0 +1,195 @@
+<?php
+namespace App\Crawler\Entities;
+
+
+class Organization
+{
+    protected $blog;
+    protected $description;
+    protected $fullName;
+    protected $id;
+    protected $lang = [];
+    protected $location;
+    protected $name;
+    protected $tags = [];
+    protected $url;
+
+
+    static public function createFromJson(string $json)
+    {
+        $object = json_decode($json);
+        $organization = new self;
+        $organization->setBlog($object->blog)
+            ->setDescription($object->description)
+            ->setFullName($object->fullName)
+            ->setId($object->id)
+            ->setLocation($object->location)
+            ->setName($object->name)
+            ->setUrl($object->url);
+
+        return $organization;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBlog()
+    {
+        return $this->blog;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLang(): array
+    {
+        return $this->lang;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $blog
+     */
+    public function setBlog($blog)
+    {
+        $this->blog = $blog;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $fullName
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @param array $lang
+     */
+    public function setLang(array $lang)
+    {
+        $this->lang = $lang;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @param array $tags
+     */
+    public function setTags(array $tags)
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+}
