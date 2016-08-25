aadf8e4de9883c946e0da9694e573fbb81463e1d
Mauro <mauropalsgraaf@hotmail.com>
2016-08-25 10:30:55 +0200

    Add dependencies in Dependency container and adjust dockerfile to add file system permisisons

--- src/dependencies.php
+++ src/dependencies.php
@@ -2,17 +2,22 @@
 
 // DIC configuration
 use Bacon\Controllers\UserController;
+use Bacon\Repository\Neo4jOrganizationRepository;
+use Bacon\Repository\Neo4jUserRepository;
+use GraphAware\Neo4j\OGM\EntityManager;
+
+$neo4jHost = 'http://neo4j:7474';
 
 $container = $app->getContainer();
 
 // view renderer
-$container['renderer'] = function ($c) {
+$container['Renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
     return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
 // monolog
-$container['logger'] = function ($c) {
+$container['Logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
@@ -20,6 +25,19 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
-$container['UserController'] = function ($container) {
-    return new UserController(null);
+$container['EntityManager'] = function ($c) use ($neo4jHost) {
+    return EntityManager::create($neo4jHost, '../var/cache');
 };
+
+$container['Neo4jOrganisationRepository'] = function ($c) {
+    return new Neo4jOrganizationRepository($c['EntityManager']);
+};
+
+$container['Neo4jUserRepository'] = function ($c) {
+    return new Neo4jUserRepository($c['EntityManager']);
+};
+
+$container['UserController'] = function ($c) {
+    return new UserController($c['Neo4jUserRepository']);
+};
+
